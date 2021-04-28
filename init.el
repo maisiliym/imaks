837c69c3cb442813bc82926153777d66762847ce
li <@li:maisiliym.uniks>
2021-04-28 08:46:26 +0700

    (snap)

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(setq custom-file "~/.config/emacs/custom-file.el")
+(load custom-file)
 (electric-pair-mode 1)
 (menu-bar-mode -1)
 (load-theme 'tango t)
@@ -22,36 +24,3 @@
 (require 'projectile)
 (projectile-mode +1)
 (setq projectile-project-search-path '("~/git/"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((eval modify-syntax-entry 43 "'")
-     (eval modify-syntax-entry 36 "'")
-     (eval modify-syntax-entry 126 "'")
-     (eval let
-	   ((root-dir-unexpanded
-	     (locate-dominating-file default-directory ".dir-locals.el")))
-	   (when root-dir-unexpanded
-	     (let*
-		 ((root-dir
-		   (expand-file-name root-dir-unexpanded))
-		  (root-dir*
-		   (directory-file-name root-dir)))
-	       (unless
-		   (boundp 'geiser-guile-load-path)
-		 (defvar geiser-guile-load-path 'nil))
-	       (make-local-variable 'geiser-guile-load-path)
-	       (require 'cl-lib)
-	       (cl-pushnew root-dir* geiser-guile-load-path :test #'string-equal))))
-     (eval setq-local guix-directory
-	   (locate-dominating-file default-directory ".dir-locals.el")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
