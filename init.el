112bec18076663747e7d9259ec5ea21ed7203583
li <@li:maisiliym.uniks>
2021-05-12 11:30:22 +0700

    (snap initCustomXah)

--- init.el
+++ init.el
@@ -6,20 +6,29 @@
 (scroll-bar-mode -1)
 (delete-selection-mode 1)
 
+(require 'xah-fly-keys)
 (defun start-xah-fly-keys ()
   (xah-fly-keys 1)
-  (xah-fly-keys-set-layout "colemak"))
+  (xah-fly-keys-set-layout "colemak")
+  (define-key xah-fly-leader-key-map (kbd "t") 'consult-buffer)
+  (define-key xah-fly-key-map (kbd "k") 'consult-line))
 
-(require 'xah-fly-keys)
-(if (daemonp)
-    (add-hook 'server-after-make-frame-hook 'start-xah-fly-keys)
-  (start-xah-fly-keys))
+(defun my-config-xah-fly-key ()
+  "Modify keys for xah fly key command mode keys
+To be added to `xah-fly-command-mode-activate-hook'"
+  (interactive))
+
+(add-hook 'xah-fly-command-mode-activate-hook 'my-config-xah-fly-key)
 
 (defun my-xfk-addon-insert ()
   "Modify keys for xah fly key command mode keys To be added to `xah-fly-insert-mode-activate-hook'"
   (interactive))
 
 (add-hook 'xah-fly-insert-mode-activate-hook 'my-xfk-addon-insert)
+
+(if (daemonp)
+    (add-hook 'server-after-make-frame-hook 'start-xah-fly-keys)
+  (start-xah-fly-keys))
 
 (electric-pair-mode 1)
 (pinentry-start)
@@ -64,7 +73,7 @@
 ;; (add-hook 'minibuffer-exit-hook 'posframe-delete-all)
 
 (require 'magit)
-(git-gutter-mode)
+(global-git-gutter-mode)
 
 (require 'projectile)
 (projectile-mode +1)
